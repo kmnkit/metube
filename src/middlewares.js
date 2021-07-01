3ee65b491dad20d5e8b0de31c8fb98a38de3d9f5
Marco Kang <kmnkit@gmail.com>
2021-07-01 20:27:29 +0900

    edit middleware

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,25 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+
+const s3 = new aws.S3({
+  credentials: {
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET,
+  },
+});
+
+const s3ImageUploader = multerS3({
+  s3: s3,
+  bucket: "wetube-marco/images",
+  acl: "public-read",
+});
+
+const s3VideoUploader = multerS3({
+  s3: s3,
+  bucket: "wetube-marco/videos",
+  acl: "public-read",
+});
 
 const isHeroku = process.env.NODE_ENV === "production";
 
